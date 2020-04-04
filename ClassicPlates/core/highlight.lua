b298a977dac99bb453976247d1b24238809778bc
acidtrips <34413241+acidtrips@users.noreply.github.com>
2020-04-03 20:20:04 -0600

    Fixed OnUpdate overwrite, renamed helper function to IsUnitMousedOver

--- ClassicPlates/core/highlight.lua
+++ ClassicPlates/core/highlight.lua
@@ -8,17 +8,17 @@
 local UnitExists, UnitIsUnit = UnitExists, UnitIsUnit
 
 
-local function UnitIsMousedOver(unit)
-  return (UnitExists("mouseover") and UnitIsUnit("mouseover", unit) and not UnitIsUnit("target", unit))
+local function IsUnitMousedOver(unit)
+  return UnitExists("mouseover") and UnitIsUnit("mouseover", unit) and not UnitIsUnit("target", unit)
 end
 
 
-function NamePlateMixin:Highlight_OnUpdate(elapsed)
+function NamePlateMixin:HighlightOnUpdate(elapsed)
   self.elapsed = (self.elapsed + elapsed)
   if ( self.elapsed >= 0.1 ) then
-    if ( not UnitIsMousedOver(self.unit) ) then
+    if ( not IsUnitMousedOver(self.unit) ) then
       self.isHighlighted = false
-      self:Update_NameColor()
+      self:UpdateNameColor()
       self:SetOnUpdate(nil)
     end
     self.elapsed = 0
@@ -26,10 +26,12 @@ function NamePlateMixin:Highlight_OnUpdate(elapsed)
 end
 
 
-function NamePlateMixin:Update_Highlight()
-  if ( UnitIsMousedOver(self.unit) and not self.isInCombat ) then
-    self.isHighlighted = true
-    self:Update_NameColor()
-    self:SetOnUpdate(self.Highlight_OnUpdate, false)
+function NamePlateMixin:UpdateHighlight()
+  if ( not self.isHighlighted ) then
+    if ( IsUnitMousedOver(self.unit) and not self.isInCombat ) then
+      self.isHighlighted = true
+      self:UpdateNameColor()
+      self:SetOnUpdate(self.HighlightOnUpdate, nil)
+    end
   end
 end
