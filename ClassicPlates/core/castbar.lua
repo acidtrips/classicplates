765d3932cafc76f0fa8f69afb86e1d74fcf79d8c
acidtrips <34413241+acidtrips@users.noreply.github.com>
2020-04-03 20:22:47 -0600

    Major refactoring to make extensive use of mixin, Improved init code

--- ClassicPlates/core/castbar.lua
+++ ClassicPlates/core/castbar.lua
@@ -2,7 +2,7 @@
 
     castbar.lua
 
-    Contains functions that drive the castbar.
+    Contains functions that drive the nameplate castbar frames.
 
 --]]
 local UnitIsUnit, UnitIsPlayer, UnitCastingInfo, UnitChannelInfo, GetTime =
@@ -11,13 +11,26 @@ local UnitIsUnit, UnitIsPlayer, UnitCastingInfo, UnitChannelInfo, GetTime =
 CastBarMixin = {}
 
 
-function CastBarMixin:StopCasting(holdTime)
-  if ( not UnitIsPlayer(self.unit) ) then
-    self.holdTime = holdTime and (GetTime() + 1) or 0
-    self.fadeOut = true
+function CastBarMixin:ShouldShowCastBar(options)
+  if ( options.showCastBars ) then
+    if ( options.showCastBarsTargetOnly ) then
+      return UnitIsUnit("target", self.unit)
+    end
+    return true
+  end
+  return false
+end
+
+
+function CastBarMixin:UpdateIsShown(options)
+  self.showCastBar = self:ShouldShowCastBar(options)
+  if ( self.showCastBar and (self.isCasting or self.isChanneling) ) then
+    self:Initialize()
   else
     self:Hide()
   end
+  self.CastBarTextBG:SetShown(options.showCastBarsSpellName)
+  self.CastBarText:SetShown(options.showCastBarsSpellName)
 end
 
 
@@ -29,26 +42,131 @@ function CastBarMixin:UpdateBorder(notInterruptible)
 end
 
 
-function CastBarMixin:ShouldShowCastBar()
-  if ( ClassicPlatesDB.showCastBars ) then
-    if ( ClassicPlatesDB.showCastBarsTargetOnly ) then
-      return UnitIsUnit("target", self:GetParent().unit)
-    end
-    return true
+function CastBarMixin:CastStart()
+  local name, text, texture, startTime, endTime, isTradeSkill, castID, notInterruptible = UnitCastingInfo(self.unit)
+  if ( not name or isTradeSkill ) then
+    self:Hide()
+    return
   end
-  return false
+  self.value = (GetTime() - (startTime / 1000))
+  self.maxValue = ((endTime - startTime) / 1000)
+  self:SetMinMaxValues(0, self.maxValue)
+  self:SetValue(self.value)
+  self.CastBarText:SetText(text or name)
+  self.CastBarSpellIcon:SetTexture(texture)
+  self:SetAlpha(1.0)
+  self.fadeOut = nil
+  self.holdTime = 0
+  self.castID = castID
+  self.isCasting = true
+  self.isChanneling = nil
+  self:UpdateBorder(notInterruptible)
+  if ( self.showCastBar ) then
+    self:Show()
+  end
 end
 
 
-function CastBarMixin:UpdateIsShown()
-  self.showCastBar = self:ShouldShowCastBar()
-  if ( self.showCastBar and (self.isCasting or self.isChanneling) ) then
-    self:OnEvent("PLAYER_ENTERING_WORLD")
+function CastBarMixin:CastChannelStart()
+  local name, text, texture, startTime, endTime, isTradeSkill, notInterruptible = UnitChannelInfo(self.unit)
+  if ( not name or isTradeSkill ) then
+    self:Hide()
+    return
+  end
+  self.value = ((endTime / 1000) - GetTime())
+  self.maxValue = ((endTime - startTime) / 1000)
+  self:SetMinMaxValues(0, self.maxValue)
+  self:SetValue(self.value)
+  self.CastBarText:SetText(text or name)
+  self.CastBarSpellIcon:SetTexture(texture)
+  self:SetAlpha(1.0)
+  self.fadeOut = nil
+  self.holdTime = 0
+  self.isCasting = nil
+  self.isChanneling = true
+  self:UpdateBorder(notInterruptible)
+  if ( self.showCastBar ) then
+    self:Show()
+  end
+end
+
+
+function CastBarMixin:CastStop(castID)
+  if ( not self:IsVisible() ) then
+    self:Hide()
+  end
+  if ( self.isCasting and castID == self.castID ) then
+    self:SetValue(self.maxValue)
+    self.isCasting = nil
+    self:CastFinish()
+  end
+end
+
+
+function CastBarMixin:CastChannelStop()
+  if ( not self:IsVisible() ) then
+    self:Hide()
+  end
+  if ( self.isChanneling ) then
+    self:SetValue(self.maxValue)
+    self.isChanneling = nil
+    self:CastFinish()
+  end
+end
+
+
+function CastBarMixin:CastFail(event, castID)
+  if ( self:IsShown() and (self.isCasting and castID == self.castID) and not self.fadeOut ) then
+    self:SetValue(self.maxValue)
+    self.CastBarText:SetText((event == "UNIT_SPELLCAST_FAILED") and "Failed" or "Interrupted")
+    self.isCasting = nil
+    self.isChanneling = nil
+    self:CastFinish(true)
+  end
+end
+
+
+function CastBarMixin:CastDelayed()
+  if ( self:IsShown() ) then
+    local name, _, _, startTime, endTime, isTradeSkill = UnitCastingInfo(self.unit)
+    if ( not name or isTradeSkill ) then
+      self:Hide()
+      return
+    end
+    self.value = (GetTime() - (startTime / 1000))
+    self.maxValue = ((endTime - startTime) / 1000)
+    self:SetMinMaxValues(0, self.maxValue)
+    if ( not self.isCasting ) then
+      self.isCasting = true
+      self.isChanneling = nil
+      self.fadeOut = nil
+    end
+  end
+end
+
+
+function CastBarMixin:CastChannelUpdate()
+  if ( self:IsShown() ) then
+    local name, _, _, startTime, endTime, isTradeSkill = UnitChannelInfo(self.unit)
+    if ( not name or isTradeSkill ) then
+      self:Hide()
+      return
+    end
+    self.value = ((endTime / 1000) - GetTime())
+    self.maxValue = ((endTime - startTime) / 1000)
+    self:SetMinMaxValues(0, self.maxValue)
+    self:SetValue(self.value)
+  end
+end
+
+
+function CastBarMixin:CastFinish(holdTime)
+  if ( not UnitIsPlayer(self.unit) ) then
+    self.holdTime = holdTime and (GetTime() + 1) or 0
+    self.fadeOut = true
   else
     self:Hide()
   end
-  self.CastBarTextBG:SetShown(ClassicPlatesDB.showCastBarsSpellName)
-  self.CastBarText:SetShown(ClassicPlatesDB.showCastBarsSpellName)
 end
 
 
@@ -60,22 +178,35 @@ function CastBarMixin:SetUnit(unit)
     self.holdTime = 0
     self.fadeOut = nil
     if ( unit ) then
-      self:RegisterEvent("UNIT_SPELLCAST_INTERRUPTED")
-      self:RegisterEvent("UNIT_SPELLCAST_DELAYED")
-      self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START")
-      self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_UPDATE")
-      self:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP")
-      self:RegisterEvent("UNIT_SPELLCAST_INTERRUPTIBLE")
-      self:RegisterEvent("UNIT_SPELLCAST_NOT_INTERRUPTIBLE")
-      self:RegisterEvent("PLAYER_ENTERING_WORLD")
+      self:RegisterUnitEvent("UNIT_SPELLCAST_INTERRUPTED", unit)
+      self:RegisterUnitEvent("UNIT_SPELLCAST_DELAYED", unit)
+      self:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_START", unit)
+      self:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_UPDATE", unit)
+      self:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_STOP", unit)
+      self:RegisterUnitEvent("UNIT_SPELLCAST_INTERRUPTIBLE", unit)
+      self:RegisterUnitEvent("UNIT_SPELLCAST_NOT_INTERRUPTIBLE", unit)
       self:RegisterUnitEvent("UNIT_SPELLCAST_START", unit)
       self:RegisterUnitEvent("UNIT_SPELLCAST_STOP", unit)
       self:RegisterUnitEvent("UNIT_SPELLCAST_FAILED", unit)
-      self:OnEvent("PLAYER_ENTERING_WORLD")
+      self:RegisterEvent("PLAYER_ENTERING_WORLD")
+      self:Initialize()
     else
       self:UnregisterAllEvents()
       self:Hide()
     end
+  end
+end
+
+
+function CastBarMixin:Initialize()
+  if ( UnitChannelInfo(self.unit) ) then
+    self:CastChannelStart()
+  elseif ( UnitCastingInfo(self.unit) ) then
+    self:CastStart()
+  else
+    self.isCasting = nil
+    self.isChanneling = nil
+    self:CastFinish()
   end
 end
 
@@ -98,106 +229,24 @@ end
 
 
 function CastBarMixin:OnEvent(event, ...)
-  local unit = ...
   if ( event == "PLAYER_ENTERING_WORLD" ) then
-    if ( UnitChannelInfo(self.unit) ) then
-      event = "UNIT_SPELLCAST_CHANNEL_START"
-      unit = self.unit
-    elseif ( UnitCastingInfo(self.unit) ) then
-      event = "UNIT_SPELLCAST_START"
-      unit = self.unit
-    else
-      self.isCasting = nil
-      self.isChanneling = nil
-      self:StopCasting()
-    end
-  end
-  if ( unit ~= self.unit ) then
-    return
-  end
-  if ( event == "UNIT_SPELLCAST_START" or event == "UNIT_SPELLCAST_CHANNEL_START" ) then
-    local name, text, texture, startTime, endTime, isTradeSkill, castID, notInterruptible
-    if ( event == "UNIT_SPELLCAST_CHANNEL_START" ) then
-      name, text, texture, startTime, endTime, isTradeSkill, notInterruptible = UnitChannelInfo(unit)
-      self.value = ((endTime / 1000) - GetTime())
-      self.maxValue = ((endTime - startTime) / 1000)
-      self.isCasting = nil
-      self.isChanneling = true
-    else
-      name, text, texture, startTime, endTime, isTradeSkill, castID, notInterruptible = UnitCastingInfo(unit)
-      self.value = (GetTime() - (startTime / 1000))
-      self.maxValue = ((endTime - startTime) / 1000)
-      self.isCasting = true
-      self.isChanneling = nil
-      self.castID = castID
-    end
-    if ( not name or isTradeSkill ) then
-      self:Hide()
-      return
-    end
-    self:SetMinMaxValues(0, self.maxValue)
-    self:SetValue(self.value)
-    self.CastBarText:SetText(text)
-    self.CastBarSpellIcon:SetTexture(texture)
-    self:SetAlpha(1.0)
-    self.fadeOut = nil
-    self.holdTime = 0
-    self:UpdateBorder(notInterruptible)
-    if ( self.showCastBar ) then
-      self:Show()
-    end
-  elseif ( event == "UNIT_SPELLCAST_STOP" or event == "UNIT_SPELLCAST_CHANNEL_STOP" ) then
+    self:Initialize()
+  elseif ( event == "UNIT_SPELLCAST_START" ) then
+    self:CastStart()
+  elseif ( event == "UNIT_SPELLCAST_CHANNEL_START" ) then
+    self:CastChannelStart()
+  elseif ( event == "UNIT_SPELLCAST_STOP" ) then
     local _, castID = ...
-    if ( not self:IsVisible() ) then
-      self:Hide()
-    end
-    if ( (self.isCasting and event == "UNIT_SPELLCAST_STOP" and castID == self.castID) or
-         (self.isChanneling and event == "UNIT_SPELLCAST_CHANNEL_STOP") ) then
-      self:SetValue(self.maxValue)
-      if ( event == "UNIT_SPELLCAST_STOP" ) then
-        self.isCasting = nil
-      else
-        self.isChanneling = nil
-      end
-      self:StopCasting()
-    end
+    self:CastStop(castID)
+  elseif ( event == "UNIT_SPELLCAST_CHANNEL_STOP" ) then
+    self:CastChannelStop()
   elseif ( event == "UNIT_SPELLCAST_FAILED" or event == "UNIT_SPELLCAST_INTERRUPTED" ) then
     local _, castID = ...
-    if ( self:IsShown() and (self.isCasting and castID == self.castID) and not self.fadeOut ) then
-      self:SetValue(self.maxValue)
-      self.CastBarText:SetText((event == "UNIT_SPELLCAST_FAILED") and "Failed" or "Interrupted")
-      self.isCasting = nil
-      self.isChanneling = nil
-      self:StopCasting(true)
-    end
+    self:CastFail(event, castID)
   elseif ( event == "UNIT_SPELLCAST_DELAYED" ) then
-    if ( self:IsShown() ) then
-      local name, _, _, startTime, endTime, isTradeSkill = UnitCastingInfo(unit)
-      if ( not name or isTradeSkill ) then
-        self:Hide()
-        return
-      end
-      self.value = (GetTime() - (startTime / 1000))
-      self.maxValue = ((endTime - startTime) / 1000)
-      self:SetMinMaxValues(0, self.maxValue)
-      if ( not self.isCasting ) then
-        self.isCasting = true
-        self.isChanneling = nil
-        self.fadeOut = nil
-      end
-    end
+    self:CastDelayed()
   elseif ( event == "UNIT_SPELLCAST_CHANNEL_UPDATE" ) then
-    if ( self:IsShown() ) then
-      local name, _, _, startTime, endTime, isTradeSkill = UnitChannelInfo(unit)
-      if ( not name or isTradeSkill ) then
-        self:Hide()
-        return
-      end
-      self.value = ((endTime / 1000) - GetTime())
-      self.maxValue = ((endTime - startTime) / 1000)
-      self:SetMinMaxValues(0, self.maxValue)
-      self:SetValue(self.value)
-    end
+    self:CastChannelUpdate()
   elseif ( event == "UNIT_SPELLCAST_INTERRUPTIBLE" or event == "UNIT_SPELLCAST_NOT_INTERRUPTIBLE" ) then
     self:UpdateBorder((event == "UNIT_SPELLCAST_NOT_INTERRUPTIBLE"))
   end
@@ -211,7 +260,7 @@ function CastBarMixin:OnUpdate(elapsed)
       self:SetValue(self.maxValue)
       self.isCasting = nil
       self.isChanneling = nil
-      self:StopCasting()
+      self:CastFinish()
       return
     end
     self:SetValue(self.value)
@@ -220,7 +269,7 @@ function CastBarMixin:OnUpdate(elapsed)
     if ( self.value <= 0 ) then
       self.isCasting = nil
       self.isChanneling = nil
-      self:StopCasting()
+      self:CastFinish()
       return
     end
     self:SetValue(self.value)
@@ -235,6 +284,8 @@ function CastBarMixin:OnUpdate(elapsed)
       self:Hide()
     end
   else
+    self.isCasting = nil
+    self.isChanneling = nil
     self:Hide()
   end
 end
